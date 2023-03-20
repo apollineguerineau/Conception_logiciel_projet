b7e46290da0e5a70dcb55007f07d74cbfaec7128
aguerineau <apolline.guerineau@eleve.ensai.fr>
2023-03-20 14:59:22 +0000

    modifs

--- meteo.py
+++ meteo.py
@@ -1,3 +1,4 @@
+import consts as cs
 import requests
 
 
@@ -10,14 +11,4 @@ class Meteo:
         r = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,weathercode&current_weather=true')
         response = r.json()['current_weather']
         code = response['weathercode']
-        return (response['temperature'], codes[code])
-
-
-codes = {0: "clear sky", 1: "mainly clear, partly cloudy, and overcast", 2: "mainly clear, partly cloudy, and overcast", 3: "mainly clear, partly cloudy, and overcast",
-         45: "fog and depositing rime fog", 48: "fog and depositing rime fog", 51: "Drizzle : Light, moderate, and dense intensity", 53: "Drizzle : Light, moderate, and dense intensity",
-         55: "Drizzle : Light, moderate, and dense intensity", 56: "Freezing Drizzle: Light and dense intensity", 57: "Freezing Drizzle: Light and dense intensity",
-         61: "Rain: slight, moderate and heavy intensity", 63: "Rain: slight, moderate and heavy intensity", 65: "Rain: slight, moderate and heavy intensity",
-         66: "freezing rain:light and heavy intensity", 67: "freezing rain:light and heavy intensity", 71: "Snow fall : slight, moderate and heavy intensity",
-         73: "Snow fall : slight, moderate and heavy intensity", 75: "Snow fall : slight, moderate and heavy intensity", 77: "snow grains",
-         80: "Rain showers", 81: "Rain showers", 82: "Rain showers", 85: "Snow showers", 86: "Snow showers", 95: "thunderstorm", 96: "thunderstorm with slight and heavy hail",
-         99: "thunderstorm with slight and heavy hail"}
+        return (response['temperature'], cs.codes[code])
