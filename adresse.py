36aa3ab8de27128b12f862ecb8de177b394311cf
aguerineau <apolline.guerineau@eleve.ensai.fr>
2023-03-20 15:53:55 +0000

    séance 20/03, finalisation partie Python

--- adresse.py
+++ adresse.py
@@ -1,6 +1,11 @@
 from bulk_geocoding import geocode
 
 
+class Adresse_Exception(Exception):
+    def __str__(self):
+        return ("L'adresse n'existe pas")
+
+
 class Adresse:
     def __init__(self, numero, type_voie, nom_voie, code_postal):
         self.numero = numero
@@ -18,7 +23,9 @@ class Adresse:
                 ],
             columns=["numero", "type_voie", "nom_voie"],
             citycode="code_postal"
-        )
+                )
+        if geocoded[0]['result_status'] == 'not-found':
+            raise Adresse_Exception()
         latitude = float(geocoded[0]['latitude'])
         longitude = float(geocoded[0]['longitude'])
         return ([latitude, longitude])
---
+++ adresse_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from adresse import Adresse
+
+
+class AdresseTest(unittest.TestCase):
+    def test_donne_latitude_longitude(self):
+        adresse = Adresse(10, "Place", "Paul et Jean-Paul Avisseau", 33300)
+        expected = [45.259605, -1.078761]
+        actual = adresse.donne_latitude_longitude
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
