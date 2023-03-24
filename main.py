98b254750d5f5f55b0dd9767f1d6fbaa6c85b075
Gaulthier <gaulthier.voland@eleve.ensai.fr>
2023-03-24 16:26:47 +0100

    finalisation fichier main

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
-from adresse import Adresse, Adresse_Exception
+from flask import Flask, request, render_template
+from view import View
 from meteo import Meteo
-from pydantic import BaseModel
-from fastapi import FastAPI
-import uvicorn
+from adresse import *
 
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route('/')
+def home():
+    return View.home()
 
+@app.route('/address', methods=['POST'])
+def address():
+    adress_number = request.form['num']
+    adress_type = request.form['type_voie']
+    adress_name = request.form['nom_voie']
+    adress_postal = request.form['code_postal']
 
-class AdresseCreation(BaseModel):
-    # pylint: disable=too-few-public-methods
-    """classe PartieCreation pour la transmission d'informations
+    print(f"L'adresse est : {adress_number}, {adress_type} {adress_name}, {adress_postal}")
 
-    Parameters
-    ----------
-    BaseModel : BaseModel
-        classe mère
-    """
-    numero: int
-    type_voie: str
-    nom_voie: str
-    code_postal: int
+    user_adress = Adresse(int(adress_number), str(adress_type), str(adress_name), int(adress_postal))
 
-
-@app.get("/is_adresse")
-async def existing_adresse(adresse: AdresseCreation):
-    numero = adresse.numero
-    type_voie = adresse.type_voie
-    nom_voie = adresse.nom_voie
-    code_postal = adresse.code_postal
-    adresse = Adresse(numero, type_voie, nom_voie, code_postal)
-    try:
-        adresse.donne_latitude_longitude()
-    except Adresse_Exception:
-        return (False)
-    return (True)
-
-
-@app.get("/adresse")
-async def get_meteo(adresse: AdresseCreation):
-    numero = adresse.numero
-    type_voie = adresse.type_voie
-    nom_voie = adresse.nom_voie
-    code_postal = adresse.code_postal
-    adresse = Adresse(numero, type_voie, nom_voie, code_postal)
-    meteo = Meteo(adresse)
-    return (meteo.donne_meteo())
+    meteo = Meteo(user_adress)
+    temp, code = meteo.donne_meteo()
+    adresse = f"{adress_number}, {adress_type} {adress_name}, {adress_postal}"
+    print(f"La température est de {temp}°C.")
+    return View.functions[code](adresse, temp)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    app.run(debug=True)
